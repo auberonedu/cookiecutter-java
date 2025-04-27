1757c76383bfd704a79b59f3733caafd9013735f
Auberon López <auberonedu@gmail.com>
2025-04-27 15:46:42 -0700

    Moving directory earlier to lessen lock contention

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,9 @@ import shutil
 def flatten():
     project_dir = os.getcwd()  # .../demo
     parent = os.path.dirname(project_dir)  # one level up
+
+    os.chdir(parent)  # <-- Move up immediately to avoid lock!
+
     for name in os.listdir(project_dir):
         src = os.path.join(project_dir, name)
         dst = os.path.join(parent, name)
@@ -11,9 +14,8 @@ def flatten():
         if os.path.exists(dst):
             continue
         shutil.move(src, dst)
-    # go up and remove the empty demo/ folder
-    os.chdir(parent)
-    # shutil.rmtree(project_dir)
+
+    shutil.rmtree(project_dir)  # Now safe to delete
 
 if __name__ == "__main__":
     flatten()
