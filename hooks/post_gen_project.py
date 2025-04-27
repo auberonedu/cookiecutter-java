54c66cf54c73e9d996f1d9ef8cf74e791f4333ee
Auberon López <auberonedu@gmail.com>
2025-04-27 15:43:12 -0700

    re-enabling most of flatten

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,19 +1,19 @@
-# import os
-# import shutil
+import os
+import shutil
 
-# def flatten():
-#     project_dir = os.getcwd()  # .../demo
-#     parent = os.path.dirname(project_dir)  # one level up
-#     for name in os.listdir(project_dir):
-#         src = os.path.join(project_dir, name)
-#         dst = os.path.join(parent, name)
-#         # skip if somehow already exists
-#         if os.path.exists(dst):
-#             continue
-#         shutil.move(src, dst)
-#     # go up and remove the empty demo/ folder
-#     os.chdir(parent)
-#     shutil.rmtree(project_dir)
+def flatten():
+    project_dir = os.getcwd()  # .../demo
+    parent = os.path.dirname(project_dir)  # one level up
+    for name in os.listdir(project_dir):
+        src = os.path.join(project_dir, name)
+        dst = os.path.join(parent, name)
+        # skip if somehow already exists
+        if os.path.exists(dst):
+            continue
+        shutil.move(src, dst)
+    # go up and remove the empty demo/ folder
+    os.chdir(parent)
+    # shutil.rmtree(project_dir)
 
-# if __name__ == "__main__":
-#     flatten()
+if __name__ == "__main__":
+    flatten()
